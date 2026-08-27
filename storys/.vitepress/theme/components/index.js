66948037e7775eb56b56f1263ed0b355da10dbb4
cwbon1 <charliewbond@gmail.com>
2026-08-27 23:36:02 +1000

    Revert "Create index.js"
    This reverts commit c9d9e902ad4941185c94b2ed2dcce47399c7f7c3.

--- storys/.vitepress/theme/components/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import SnakeGame from './components/SnakeGame.vue'
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('SnakeGame', SnakeGame)
-  }
-}
