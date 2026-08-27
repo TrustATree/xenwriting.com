6c7278ab5baaa390ca8e47535aa0d5bbcf2153bd
Charlie <129813033+Charl1e39@users.noreply.github.com>
2026-08-27 22:46:04 +1000

    Update config.mjs

--- storys/.vitepress/config.mjs
+++ storys/.vitepress/config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
         { text: 'Discord', link: 'https://discord.com/invite/et2Yyyn62p' }
         ],
       copyright: 'Copyright © 2026 TreeXen'
-  },
+  }
   vite: {
     resolve: {
       alias: [
